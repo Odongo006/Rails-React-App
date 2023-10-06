70a13473b6431af1ef4f77da324ef3a27cbc9195
Odongo006 <henryomondi6612@gmail.com>
2023-10-06 23:03:15 +0300

    Add dev branch

--- app/controllers/api/greetings_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::GreetingsController < ApplicationController
-  def index
-    @greeting = Greeting.order('RANDOM()').first
-    render json: @greeting.message
-  end
-end
--- app/controllers/greetings_controller.rb
+++
@@ -1,5 +0,0 @@
-class GreetingsController < ApplicationController
-  def index
-    @greeting = Greeting.order('RANDOM()').first
-  end
-end
